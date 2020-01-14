27e5cccf2519063ed7f0722d255aa6f46ee716d6
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2020-01-14 19:11:23 +0500

    server js updated

--- server.js
+++ server.js
@@ -137,11 +137,15 @@ app.post('/api/searchcolors', async (req, res, next) => {
   res.status(200).json(ret);
 });
 
-// Server static assets if in production
-app.use('/', express.static(path.join(__dirname, 'client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
-});
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'))
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) // relative path
+  })
+}
 
 const port = process.env.PORT || 5000;
 
